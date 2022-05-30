51851a939e1d5c0ab048f480100e626592aa1724
Stennsen <74929916+Stennsen@users.noreply.github.com>
2022-05-31 00:40:27 +0200

    tidied up Code, added User interaction

--- main.py
+++ main.py
@@ -10,31 +10,23 @@ jumpDict = {1: {6, 8},              # this variable stores which numbers are rea
             0: {4, 6}}
 
 def jump(number, hops):
-    print("Hops left " + str(hops) + ": " + str(number))
     if hops == 0 or number[len(number)-1]== 5:
         distinctNumbers.add(str(number))
-        print("Numberpool: " + str(distinctNumbers))
     else:
         for j in jumpDict[number[len(number)-1]]:
-            print("jump to " + str(j))
             number.append(j)
             jump(number, hops-1)
             number.pop()
-            print(number)
 
 def getDisctinctNumbers(start, hops):
     global distinctNumbers
     distinctNumbers = set()
     number = [start]
     jump(number, hops)
-    print(distinctNumbers)
     return len(distinctNumbers)
 
-
-def test():                             # second Test: checks wether n=2 for start=1 works - it should be 5 distinct numbers
-    if getDisctinctNumbers(1, 2) == 5:
-        print("Test passed")
-    else:
-        print("Test failed")
-
-test()
+print("Enter starting number:")
+start = int(input())
+print("Enter amount of jumps:")
+jumps = int(input())
+print(str(getDisctinctNumbers(start, jumps)) + " differnt numbers are possible.")
