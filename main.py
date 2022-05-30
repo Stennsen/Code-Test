4fca08ff6587fd30f56eec962c463f92bbe1b1a2
Stennsen <74929916+Stennsen@users.noreply.github.com>
2022-05-31 00:32:22 +0200

    Noticed Error in Test #2, it should be 5 distinct Numbers. Reimplemented Task as recursive function. Passed Test #2

--- main.py
+++ main.py
@@ -9,25 +9,30 @@ jumpDict = {1: {6, 8},              # this variable stores which numbers are rea
             9: {2, 4},
             0: {4, 6}}
 
-
+def jump(number, hops):
+    print("Hops left " + str(hops) + ": " + str(number))
+    if hops == 0 or number[len(number)-1]== 5:
+        distinctNumbers.add(str(number))
+        print("Numberpool: " + str(distinctNumbers))
+    else:
+        for j in jumpDict[number[len(number)-1]]:
+            print("jump to " + str(j))
+            number.append(j)
+            jump(number, hops-1)
+            number.pop()
+            print(number)
 
 def getDisctinctNumbers(start, hops):
+    global distinctNumbers
     distinctNumbers = set()
     number = [start]
-    for i in range(hops):
-        if start == 5:
-            distinctNumbers.add(str(number))
-        else:
-            for j in jumpDict[start]:
-                number.append(j)
-                if i == hops-1:
-                    distinctNumbers.add(str(number))
-                number.pop()
+    jump(number, hops)
+    print(distinctNumbers)
     return len(distinctNumbers)
 
 
-def test():                             # second Test: checks wether n=2 for start=1 works - it should be 4 distinct numbers
-    if getDisctinctNumbers(start, hops)(1, 2) == 4:
+def test():                             # second Test: checks wether n=2 for start=1 works - it should be 5 distinct numbers
+    if getDisctinctNumbers(1, 2) == 5:
         print("Test passed")
     else:
         print("Test failed")
